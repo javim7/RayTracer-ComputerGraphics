9ed6ff6fac4f284e0873687ed88770d8b7ecab86
javim7 <rjmombiela@gmail.com>
2022-10-05 09:24:01 -0600

    .

--- rayTracer.py
+++ rayTracer.py
@@ -15,12 +15,16 @@ LIGHTBLUE = color(173, 216, 230)
 WHITE = color(255, 255, 255)
 BLACK = color(0, 0, 0)
 
+# codigo rt3
+MAX_RECURSION_DEPTH = 3
+# Trt3 termina aca
+
 
 class Raytracer(object):
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.background_color = WHITE
+        self.background_color = LIGHTBLUE
         self.current_color = WHITE
         self.clear()
         self.scene = []
@@ -57,7 +61,13 @@ class Raytracer(object):
 
                     self.point(x, y, c)
 
-    def cast_ray(self, origin, direction):
+    def cast_ray(self, origin, direction, recursion=0):  # recursion es el rt3
+
+        # codigo rt3
+        if recursion == MAX_RECURSION_DEPTH:
+            return self.background_color
+        # termina rt3
+
         material, intersect = self.scene_intersect(origin, direction)
 
         if material is None:
@@ -65,15 +75,41 @@ class Raytracer(object):
 
         light_dir = (self.light.position - intersect.point).normalize()
 
+        # nuevo codigo rt3
+        if material.albedo[2] > 0:
+            reverse_direction = direction * -1
+            reflect_direction = self.reflect(
+                reverse_direction, intersect.normal)
+            reflect_origin = intersect.point + \
+                (intersect.normal * 1.1)
+            reflect_color = self.cast_ray(
+                reflect_origin, reflect_direction, recursion + 1)
+        else:
+            reflect_color = color(0, 0, 0)
+
+        reflection = reflect_color * material.albedo[2]
+
+        shadow_bias = 1.1
+        shadow_origin = intersect.point + (intersect.normal * shadow_bias)
+        shadow_material, shadow_intersect = self.scene_intersect(
+            shadow_origin, light_dir)
+
+        shadow_intensity = 0
+        if shadow_material:
+            shadow_intensity = 0.7
+        # aca termina codigo rt3
+
         dIntensity = light_dir @ intersect.normal
-        diffuse = material.diffuse * dIntensity * material.albedo[0]
+        diffuse = material.diffuse * dIntensity * \
+            material.albedo[0] * \
+            (1 - shadow_intensity)  # shadow intensity es de rt3
 
         light_dir = self.reflect(light_dir, intersect.normal)
         rIntensity = max(0, light_dir @ direction)
         specularI = self.light.intensity * rIntensity ** material.spec
         specular = self.light.color * specularI * material.albedo[1]
 
-        return diffuse + specular
+        return diffuse + specular + reflection  # reflection es rt3
 
     def scene_intersect(self, origin, direction):
         zbuffer = 99999
