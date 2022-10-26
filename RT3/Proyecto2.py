e32f56dd5844fada1bd935f04a359e5c49dfd8ea
javim7 <rjmombiela@gmail.com>
2022-10-25 18:41:10 -0600

    .

--- RT3/Proyecto2.py
+++ RT3/Proyecto2.py
@@ -1,6 +1,6 @@
 from rayTracer import *
 
-ray = Raytracer(1000, 1000)
+ray = Raytracer(800, 600)
 
 rubber = Material(diffuse=color(100, 0, 0), albedo=[0.9, 0.1, 0, 0], spec=10)
 ivory = Material(diffuse=color(255, 255, 255),
@@ -10,7 +10,6 @@ mirror = Material(diffuse=color(255, 255, 255),
 glass = Material(diffuse=color(150, 180, 200), albedo=[
                  0, 0.5, 0.1, 0.8], spec=125, refractive_index=1.5)
 
-ray = Raytracer(800, 600)
 
 ray.light = Light(
     position=V3(-20, 20, 20),
@@ -19,11 +18,11 @@ ray.light = Light(
 )
 
 ray.scene = [
-    Sphere(V3(0, -1.5, -10), 1.5, ivory),
-    Sphere(V3(0, 0, -5), 0.5, glass),
-    Sphere(V3(1, 1, -8), 1.7, rubber),
-    Sphere(V3(-2, 1, -10), 2, mirror),
-    Plane(V3(0, 2.2, -5), 2, 2, glass)
+    # Sphere(V3(0, -1.5, -10), 1.5, ivory),
+    # Sphere(V3(0, 0, -5), 0.5, glass),
+    # Sphere(V3(1, 1, -8), 1.7, rubber),
+    # Sphere(V3(-2, 1, -10), 2, mirror),
+    # Plane(V3(0, 2.2, -5), 2, 2, glass)
 ]
 
 ray.envmap = Envmap('./RT3/texturas/envmap.bmp')
